128868e6c4cad450534abbeb0d0ccdcd0fce5d12
17191371munir <17191371m.sirozulmunir@gmail.com>
2022-04-06 06:43:13 +0700

    membuat tamplate sederhana dan dinamis

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama M.Sirozul Munir
+ Saya tingga di daerah Kalideres
+ olah raga yang saya sukai adalah
+ Bulutangkisssss";
+ }
+}
